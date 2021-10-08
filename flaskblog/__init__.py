774088dc9e09dee8bb6ca4ce112cc729ea0b9a9a
yaswanthsaivendra <yaswanthsaivendra@gmail.com>
2021-10-08 23:43:59 +0530

    Intial commit

---
+++ flaskblog/__init__.py
@@ -0,0 +1,29 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from flask_mail import Mail
+
+app = Flask(__name__)
+app.config['SECRET_KEY']='089ed89a3e5797205f9bf89d63576fb8'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+bcrypt= Bcrypt(app)
+login_manager=LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = 'yaswanthsaivendra@gmail.com'
+app.config['MAIL_PASSWORD'] = 'heitqeluhmdagcgj'
+mail = Mail(app)
+
+from flaskblog.users.routes import users
+from flaskblog.posts.routes import posts
+from flaskblog.main.routes import main
+
+app.register_blueprint(users)
+app.register_blueprint(posts)
+app.register_blueprint(main)
